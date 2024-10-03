bf4a0f35851eed857f7af48b7b8c3a46018c32f8
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2024-10-03 12:55:07 +0530

    Fixed colors

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,9 @@ function App() {
       <div className='w-full h-screen  duration-200 ' style = {{background:color}}>
         <div className ='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
           <div className='flex flex-wrap justify-center bottom-12 gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-            <button onClick={()=> setColor('#E1EDEC')} className='outline-none px-4 py-1  rounded-full shadow-lg text-black font-bold'  style={{background:'#E1EDEC'}}>Misty Mint</button>
+            <button onClick={()=> setColor('#E1EDEC')} className='outline-none px-4 py-1  rounded-full shadow-lg text-black font-bold'  style={{background:'#E1EDEC'}}>Pale Cyan</button>
             <button onClick={() =>setColor('#979D99')} className='outline-none px-4 py-1  rounded-full shadow-lg text-black font-bold' style={{background:'#979D99'}}>Stone Grey</button>
-            <button onClick={()=>setColor('#A5A4B5')} className='outline-none px-4 py-1  rounded-full shadow-lg text-black font-bold' style={{background:'#A5A4B5'}}>Cloudy Blue</button>
+            <button onClick={()=>setColor('#A5A4B5')} className='outline-none px-4 py-1  rounded-full shadow-lg text-black font-bold' style={{background:'#A5A4B5'}}>Grayish Purple</button>
             <button onClick={()=>setColor('#CFADAF')} className='outline-none px-4 py-1  rounded-full shadow-lg text-black font-bold' style={{background:'#CFADAF'}}>Dusty Rose</button>
             <button onClick={()=>setColor('#7C5A66')} className='outline-none px-4 py-1  rounded-full shadow-lg text-black font-bold' style={{background:'#7C5A66'}}>Mauve Mirage</button>
           </div>
